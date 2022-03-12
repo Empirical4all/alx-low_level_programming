2b27a0daa7b965c2b4e1ea8c620b646caa33cc43
Empirical4all <empirical4all@gmail.com>
2022-03-12 05:24:07 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Return: Always 0 (Sucesses)
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-} 
+}
